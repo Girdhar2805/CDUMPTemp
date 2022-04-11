6129e46380550d7f45f37a247c6e4e19b48b85e5
Girdhar Chandrakar <49159912+Girdhar2805@users.noreply.github.com>
2022-04-11 23:03:26 +0530

    Add files via upload

---
+++ FrontEndFolder/src/components/Footer/MyFooter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MyFooter.css'
+
+export default function MyFooter() {
+  return (
+    <>
+    <footer><span className="text-muted">All Rights Reserved 2020 @CovidTrackerWebApp</span>
+    </footer>
+    </>
+  )
+}
+
+
